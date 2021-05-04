0336fa4b5f191bdb6d3cc427cf591bfcf486cc15
jgmonteiro1 <jgmoreira15@gmail.com>
2021-05-03 22:44:34 -0300

    Projeto criado

---
+++ backend/src/test/java/com/jgmonteiro/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgmonteiro.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
